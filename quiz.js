694ac68cc1cd0436ed676f0a0945f3ea1fe18571
Tom <gigatech55@gmail.com>
2015-03-19 12:37:56 +1030

    Bunch-o-functions
    Also got the darn thing to work (somewhat)

--- quiz.js
+++ quiz.js
@@ -1,29 +1,66 @@
 $(document).ready(function(){
-	var quDB = $.getJSON('questions.json');
-	var quesIDs = [];
+	//BEGIN GLOBAL VARIABLES
+	var quesIDs;
+	var curQues;
+	var totalQues;
+	var usrAns;
+	var ans;
+	//END
+	init()
 	
-	function checkAnswer(id) {
+	function init() {
+		quesIDs = [];
+		curQues = 0;
+		totalQues = 5;
+		generateQuesIDs(totalQues);
+		displayQues();
+	}
+	
+	$('#submit').click(function(){
+		if ($('input[name=selection]').is(':checked')) {
+			checkAns()
+		} else {
+			alert("Select an answer!")
+		}
+	})
+	
+	function checkAns() {
 		usrAns = $('input[name=selection]:checked').attr('id');
-		ans = quDB.questions[id].ans;
+		ans = quDB.questions[quesIDs[curQues]].ans;
+		if (usrAns == ans) {
+			alert("Correct!");
+		} else {
+			alert("Incorrect!\nThe answer was " + ans);
+		}
+		alert("You've answered " + (curQues+1) + " questions.")
+		displayQues()
 	}
 	
 	function generateQuesIDs(amt) {
 		for (var i=0;i<amt;i++) {
-			quesIDs[i] = Math.floor(Math.random()*21);
+			quesIDs[i] = Math.floor(Math.random()*20);
 		}
 	}
 	
-	function displayQues(id) {
-		var qu = quDB.questions[id].qu;
-		var c1 = quDB.questions[id].c1;
-		var c2 = quDB.questions[id].c2;
-		var c3 = quDB.questions[id].c3;
-		var c4 = quDB.questions[id].c4;
-		
-		$('.question').html(qu);
-		$('#c1').html(c1);
-		$('#c2').html(c2);
-		$('#c3').html(c3);
-		$('#c4').html(c4);
+	function displayQues() {
+		if (curQues == (totalQues-1)) {
+			finish()
+		} else {
+			curQues++;
+			var qu = quDB.questions[quesIDs[curQues]].qu;
+			var c1 = quDB.questions[quesIDs[curQues]].c1;
+			var c2 = quDB.questions[quesIDs[curQues]].c2;
+			var c3 = quDB.questions[quesIDs[curQues]].c3;
+			var c4 = quDB.questions[quesIDs[curQues]].c4;
+			$('.question').html(qu);
+			$('#labl_c1').html(c1);
+			$('#labl_c2').html(c2);
+			$('#labl_c3').html(c3);
+			$('#labl_c4').html(c4);
+		}
+	}
+	
+	function finish() {
+		alert("You've answered all the questions!");
 	}
 })
